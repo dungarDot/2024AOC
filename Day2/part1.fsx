358abea64f6674f3f716175cc920e7e2174b8bf7
dungarDot <65919488+dungarDot@users.noreply.github.com>
2024-12-04 07:41:09 -0800

    mocked up the basic report/level as defined in the challenge

--- Day2/part1.fsx
+++ Day2/part1.fsx
@@ -1,3 +1,14 @@
 open System.IO
 
-let input = File.ReadAllLines "./Day2/input.txt"
+// Shooting for a more DDD approach.  We'll see how much I stick with it.
+
+type Level = int 
+type Report  = Level array 
+type Reports = Report list
+let reports : Reports = 
+    File.ReadAllLines "./Day2/input.txt"
+    |> Seq.map(fun line ->
+        line.Split(" ")
+        |> Array.map int 
+    )
+    |> Seq.toList
