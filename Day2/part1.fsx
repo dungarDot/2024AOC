404a8d631936f3df8d2297e4574ad31dbdcfc023
dungarDot <65919488+dungarDot@users.noreply.github.com>
2024-12-06 13:51:44 -0800

    still messing around with active patterns

--- Day2/part1.fsx
+++ Day2/part1.fsx
@@ -45,7 +45,7 @@ type LevelVolatility =
     | NoChangeBetweenLevels
 
 module LevelVolatility =
-    let (|Check|) (current:Level, previous:Level) : LevelVolatility =
+    let (|CheckType|) (current:Level, previous:Level) : LevelVolatility =
         let diff = abs(current - previous)
         if diff = 0 then 
             NoChangeBetweenLevels
@@ -131,7 +131,7 @@ let rec parseReport state =
     let stability = Stability.Verify currentLevel state.PreviousLevel state.Stability
     let volatility = 
         match (currentLevel, state.PreviousLevel) with 
-        | LevelVolatility.Check lv -> lv
+        | LevelVolatility.CheckType lv -> lv
 
     checkSafety stability volatility state currentLevel state.PreviousLevel parseReport
 
